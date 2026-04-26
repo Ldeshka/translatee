ab2e4c746619f51139dbc255704969217e45fa11
Ldeshka <vinyarskyidenys@gmail.com>
2026-04-26 13:03:38 +0300

    Add files via upload

--- src/script.js
+++ src/script.js
@@ -1,26 +1,8 @@
-
-let placeholderbtn = document.querySelector(".btn")
-console.log("1")
-placeholderbtn.onclick = function() {
-    console.log("2")
-    let textFrom = document.querySelector(".from").value
-    let langFrom = document.querySelector(".lang-from").value
-    let langTo = document.querySelector(".lang-to").value
-    console.log(textFrom, langFrom, langTo)
-    translate(textFrom, langFrom, langTo)
-    
-}
-document.querySelector(".strelochki").onclick = function() {
-    console.log("4")
-    let langFromBefore = document.querySelector(".lang-from").value
-    document.querySelector(".lang-from").value = document.querySelector(".lang-to").value
-    document.querySelector(".lang-to").value = langFromBefore
-    console.log(document.querySelector(".lang-from").value)
-}
 async function translate(text, langFrom, langTo) {
-    let to = document.querySelector(".to")
     const response = await fetch(`https://api.mymemory.translated.net/get?q=${text}&langpair=${langFrom}|${langTo}`);
     let data = await response.json();
     console.log(data.responseData.translatedText)
-    to.value =  data.responseData.translatedText
 }
+translate("Hello, how are you?", "en", "uk")
+translate("Hello, do you want to hang out tomorrow? I have some free time.", "en", "zh")
+translate("Hello, can i borrow your bike? I'll return it later.", "en", "pl")
